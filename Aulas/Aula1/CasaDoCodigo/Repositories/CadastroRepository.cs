776ceb5eb2d5375cbf8a0de66320f06d972d4816
Kim Eroico <kim-eroico@hotmail.com>
2020-09-29 19:31:45 -0300

    Versão Final do Projeto Alura Formação .Net

---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/CadastroRepository.cs
@@ -0,0 +1,35 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface ICadastroRepository
+    {
+        Cadastro Update(int cadastroId, Cadastro novoCadastro);
+    }
+
+    public class CadastroRepository : BaseRepository<Cadastro>, ICadastroRepository
+    {
+        public CadastroRepository(ApplicationContext context) : base(context)
+        {
+        }
+
+        public Cadastro Update(int cadastroId, Cadastro novoCadastro)
+        {
+            var cadastroDB = dbSet.Where(c => c.Id == cadastroId)
+                .SingleOrDefault();
+
+            if (cadastroDB == null)
+            {
+                throw new ArgumentException("cadastro");
+            }
+
+            cadastroDB.Update(novoCadastro);
+            context.SaveChanges();
+            return cadastroDB;
+        }
+    }
+}
